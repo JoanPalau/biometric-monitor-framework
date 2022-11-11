4f24ab1dca04b494bd4502ed4b13a4bcd7c31de8
Joan Palau <joanp4l4u@gmail.com>
2022-11-11 16:54:37 +0100

    working connection to servo

--- src/ESP32_LOGIC/SERVO_ROUTINE.ino
+++ src/ESP32_LOGIC/SERVO_ROUTINE.ino
@@ -1,12 +1,64 @@
-// LOW Priority task defined in ESP32_LOGIC
+#include <ESP32Servo.h>
+
+#define UNCERTAIN_DEGREES 180
+#define LOW_DEGREES 0
+#define MEDIUM_DEGREES 60
+#define HIGH_DEGREES 120
+
+enum riskMonitorStatus {
+  RUNCERTAIN = 0,
+  RLOW = 1,
+  RMEDIUM = 2,
+  RHIGH = 3
+};
+
+int statusToDegrees(int riskStatus);
+
+// Medium Priority task defined in ESP32_LOGIC
 // Displays the Health Status activating a Servo
 void DisplayHealthStatus(void *pvParameters)
 {
   // Initialize the local variables here
   (void) pvParameters;
+  Servo riskMonitor;
+
+  riskMonitorStatus status = RUNCERTAIN;  
+  riskMonitor.setPeriodHertz(50);    // standard 50 hz servo
+	riskMonitor.attach(12, 500, 2400);
+  riskMonitor.write(UNCERTAIN_DEGREES);
+
   // Main routine loop
   for(;;)
   {
-    vTaskDelay(10000);
+    if(status == RUNCERTAIN) {
+        riskMonitor.write(statusToDegrees(status));
+        status = RLOW;
+    } else {
+      riskMonitor.write(30);
+      status = RUNCERTAIN;
+    }
+    
+    vTaskDelay(1000);
+  }
+}
+
+int statusToDegrees(int riskStatus)
+{
+  switch(riskStatus)
+  {
+    case RUNCERTAIN:
+      return UNCERTAIN_DEGREES;
+      break;
+    case RLOW:
+      return LOW_DEGREES;
+      break;
+    case RMEDIUM:
+      return MEDIUM_DEGREES;
+      break;
+    case RHIGH:
+      return HIGH_DEGREES;
+      break;
+    default:
+      return UNCERTAIN_DEGREES;
   }
 }
