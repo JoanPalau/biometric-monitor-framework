3fcb15b3f75d2e37eac3f6c5487bf0f1f9c2881a
Joan Palau <joanp4l4u@gmail.com>
2022-11-13 13:49:55 +0100

    Heart beating animation

--- src/ESP32_LOGIC/LCD_DISPLAY_ROUTINE.ino
+++ src/ESP32_LOGIC/LCD_DISPLAY_ROUTINE.ino
@@ -1,9 +1,9 @@
 #include <LCD_I2C.h>
-//#include <stdio.h>
 
 LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
 
-byte leftHeart[] = {
+// Define custom bytes for the lcd display
+byte leftHeartFull[] = {
   B01100,
   B11110,
   B11111,
@@ -14,7 +14,18 @@ byte leftHeart[] = {
   B00000
 };
 
-byte rightHeart[] = {
+byte leftHeartEmpty[] = {
+  B01100,
+  B10010,
+  B10001,
+  B01000,
+  B00100,
+  B00010,
+  B00001,
+  B00000
+};
+
+byte rightHeartFull[] = {
   B00110,
   B01111,
   B11111,
@@ -25,8 +36,30 @@ byte rightHeart[] = {
   B00000
 };
 
+byte rightHeartEmpty[] = {
+  B00110,
+  B01001,
+  B10001,
+  B00001,
+  B00100,
+  B01000,
+  B10000,
+  B00000
+};
+//Used to handle the animation freq.
+unsigned long heart_animation_time = millis();
+
+// Checks if there is heart rate valid data to be displayed
+bool hasValidData();
+// Displays the display title
+void printLcdTitle();
+// Displays a placeholder for heart rate values
+void printDefault();
 // Displays the Heart Rate into the lcd Screen
 void printHeartRate();
+// Displays a beating heart animation
+void printHeartAnimation();
+
 
 // Medium Priority task defined in ESP32_LOGIC
 // Displays the >Heart Rate to the LCD display
@@ -35,16 +68,26 @@ void DisplayHeartRate(void *pvParameters)
   // Initialize the local variables here
   (void) pvParameters;
   lcd.begin();
+  lcd.createChar(0, leftHeartFull);
+  lcd.createChar(1, rightHeartFull);
+  lcd.createChar(2, leftHeartEmpty);
+  lcd.createChar(3, rightHeartEmpty);
   lcd.backlight();
-
-  lcd.createChar(0, leftHeart);
-  lcd.createChar(1, rightHeart);
 
   // Main routine loop
   for(;;)
   {
     lcd.clear();
-    printHeartRate();
+    printLcdTitle();
+    lcd.setCursor(7, 1);
+    if(hasValidData())
+    {
+      printHeartRate();
+      printHeartAnimation();
+    }
+    else {
+      printDefault();
+    }
     vTaskDelay(500);
   }
 }
@@ -53,22 +96,47 @@ void printHeartRate()
 {
     char buffer[5];
     xSemaphoreTake(xHeartRateData, TICKS_TO_WAIT_HEART);
-    lcd.setCursor(3, 0);
-    lcd.print("HR Monitor");
-    lcd.setCursor(7, 1);
-    if(heartRate != -1.0){
-      sprintf(buffer, "%d", heartRate);
-      lcd.print(buffer);
-      lcd.setCursor(10, 1);
-      lcd.write(0);
-      lcd.setCursor(11, 1);
-      lcd.write(1);
-    } else {
-      sprintf(buffer, "--");
-      lcd.print(buffer);
-    }
-    
+  
+    sprintf(buffer, "%d", heartRate);
+    lcd.print(buffer);
     
     xSemaphoreGive(xHeartRateData);
-    vTaskDelay(500);
+}
+
+bool hasValidData()
+{
+  return heartRate != -1.0;
+}
+
+void printLcdTitle()
+{
+  lcd.setCursor(3, 0);
+  lcd.print("HR Monitor");
+}
+
+void printDefault()
+{
+  char buffer[5];
+  sprintf(buffer, "--");
+  lcd.print(buffer);
+}
+
+void printHeartAnimation()
+{
+  unsigned long timeSpan = millis() - heart_animation_time;
+  if(timeSpan <= 525)
+  {
+    lcd.setCursor(10, 1);
+    lcd.write(0);
+    lcd.setCursor(11, 1);
+    lcd.write(1);
+  }
+  else
+  {
+    lcd.setCursor(10, 1);
+    lcd.write(2);
+    lcd.setCursor(11, 1);
+    lcd.write(3);
+    heart_animation_time = millis();
+  }
 }
