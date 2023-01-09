b954c1ad5781b4ebc80af66fbbd035a8dd3f60fa
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2023-01-09 18:10:34 +0100

    add mqtt

---
+++ visualizer/full_mock.py
@@ -0,0 +1,25 @@
+import random
+import paho.mqtt.client as mqtt
+import time
+
+client = mqtt.Client()
+client.connect("localhost", 1883, 60)
+
+intensity = {
+    'rest': 0,
+    'walking': 1,
+    'run': 1,
+    'erratic': 2,
+    'falling': 2,
+}
+
+
+while True:
+    r = random.randint(20, 40)
+    print('Heart', r)
+    client.publish(f'sensor/heart', r)
+    act = random.choice(list(intensity.keys()))
+    r = f'0.0 0.0 0.0 {act}'
+    print('Accelerometer', r)
+    client.publish(f'sensor/accelerometer', r)
+    time.sleep(1)
