b418f8f9ca7f2583789979cbcbaa0f32eab60c9a
Joan Palau <joanp4l4u@gmail.com>
2023-01-09 19:30:38 +0100

    fixing types

--- app/pages/api/activity.tsx
+++ app/pages/api/activity.tsx
@@ -1,7 +1,8 @@
 import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next';
 const prisma = new PrismaClient()
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
       const {
           query: { },
           method,
